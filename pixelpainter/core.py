0cf92cb3412059dc52cc785a68d35be1e11f5ba8
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-04-08 20:30:18 -0400

    Update help text from --folder argument

--- pixelpainter/core.py
+++ pixelpainter/core.py
@@ -5,78 +5,83 @@ import pyperclip
 import argparse
 import os
 
+
 def user():
-	return os.path.join('C:',os.environ["HOMEPATH"], "Desktop")
+    return os.path.join('C:', os.environ["HOMEPATH"], "Desktop")
+
 
 class Image2HTML(object):
-	"""docstring for Image2HTML"""
-	def __init__(self, image, pixsize, outfolder):
-		super(Image2HTML, self).__init__()
-		self.outfolder = outfolder
-		self.IMG = Image.open(image, 'r')
-		self.IMG = self.IMG.convert("RGBA")
+    """docstring for Image2HTML"""
 
-		self.WIDTH, self.HEIGHT = self.IMG.size
+    def __init__(self, image, pixsize, outfolder):
+        super(Image2HTML, self).__init__()
+        self.outfolder = outfolder
+        self.IMG = Image.open(image, 'r')
+        self.IMG = self.IMG.convert("RGBA")
 
-		RGBC = list(self.IMG.getdata())                        # RGB COLORS
-		HEXC = ['#%02x%02x%02x%02x' % color for color in RGBC] # HEX COLORS
+        self.WIDTH, self.HEIGHT = self.IMG.size
 
-		MATRIX = np.array(HEXC).reshape(self.HEIGHT, self.WIDTH)
-		self.HTMLCODE = Array2Table(MATRIX, pixsize)
+        RGBC = list(self.IMG.getdata())                        # RGB COLORS
+        HEXC = ['#%02x%02x%02x%02x' % color for color in RGBC]  # HEX COLORS
 
-	def Save(self):
-		path = os.path.join(self.outfolder, "FILE.html")
-		FOut = open(path,"w")
-		FOut.write(self.HTMLCODE)
-		FOut.close()
-		print("HTML file saved correctly!")
+        MATRIX = np.array(HEXC).reshape(self.HEIGHT, self.WIDTH)
+        self.HTMLCODE = Array2Table(MATRIX, pixsize)
 
-	def Get(self):
-		pyperclip.copy(self.HTMLCODE)
-		print("HTML code copied correctly!")
+    def Save(self):
+        path = os.path.join(self.outfolder, "FILE.html")
+        FOut = open(path, "w")
+        FOut.write(self.HTMLCODE)
+        FOut.close()
+        print("HTML file saved correctly!")
+
+    def Get(self):
+        pyperclip.copy(self.HTMLCODE)
+        print("HTML code copied correctly!")
+
 
 def main():
-	analyser = argparse.ArgumentParser(description='Convert images to HTML.')
+    analyser = argparse.ArgumentParser(description='Convert images to HTML.')
 
-	# IMAGE PATH ARGUMENT
-	analyser.add_argument(
-		'-i', '--image', 
-		required=True,
-		type=str,
-		help="Full path from image to process."
-	)
+    # IMAGE PATH ARGUMENT
+    analyser.add_argument(
+        '-i', '--image',
+        required=True,
+        type=str,
+        help="Full path from image to process."
+    )
 
-	# PIXEL SIZE ARGUMENT
-	analyser.add_argument(
-		'-ps', '--pixelsize', 
-		default=2,
-		type=int,
-		help="Pixel size."
-	)
+    # PIXEL SIZE ARGUMENT
+    analyser.add_argument(
+        '-ps', '--pixelsize',
+        default=2,
+        type=int,
+        help="Pixel size."
+    )
 
-	# OUTPUT FOLDER
-	analyser.add_argument(
-		'-f', '--folder', 
-		default=user(),
-		type=str,
-		help="Output folder to HTML file (in case from save argument)"
-	)
+    # OUTPUT FOLDER
+    analyser.add_argument(
+        '-f', '--folder',
+        default=user(),
+        type=str,
+        help="Output folder to HTML file (in case of using -save mode in --mode argument) The default save folder is the desktop."
+    )
 
-	# MODE ARGUMENT
-	analyser.add_argument(
-		'-m', '--mode',
-		default="save",
-		choices=["save", "copy"],
-		help="Copy the generated code to clipboard (copy) or generate a HTML file (save)."
-	)
+    # MODE ARGUMENT
+    analyser.add_argument(
+        '-m', '--mode',
+        default="save",
+        choices=["save", "copy"],
+        help="Copy the generated code to clipboard (copy) or generate a HTML file (save)."
+    )
 
-	prms = analyser.parse_args()
+    prms = analyser.parse_args()
 
-	I2H = Image2HTML(prms.image, prms.pixelsize, prms.folder)
-	if prms.mode == 'copy':
-		I2H.Get()
-	elif prms.mode == 'save':
-		I2H.Save()
+    I2H = Image2HTML(prms.image, prms.pixelsize, prms.folder)
+    if prms.mode == 'copy':
+        I2H.Get()
+    elif prms.mode == 'save':
+        I2H.Save()
+
 
 if __name__ == '__main__':
-	main()
+    main()
