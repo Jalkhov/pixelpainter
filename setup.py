7cc1c905e9f7c01ca45b1a9855ebe4ea05d694cc
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-04-08 22:11:27 -0400

    Update package version

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 from setuptools import setup
 
 with open("README.md", encoding="utf-8") as f:
-	long_description = f.read()
+    long_description = f.read()
 
 setup(
-	name="pixelpainter",
-	version="0.1",
-	description="Convert images to HTML.",
-	long_description = long_description,
-	long_description_content_type="text/markdown",
-	author="Pedro Torcatt",
-	author_email="pedrotorcattsoto@gmail.com",
-	url="https://github.com/Jalkhov/pixelpainter",
-	license = "MIT",
-	keywords = "paint html pixel convert image",
-	packages=['pixelpainter'],
-	install_requires=[
-		'numpy',
-		'pyperclip'
-	],
-	entry_points={
-		'console_scripts': [
-			'pixelpainter = pixelpainter.core:main',
-		],
-	},
-	classifiers=[
-		"Topic :: Utilities",
-		"Environment :: Console",
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: Microsoft :: Windows",
-		"Development Status :: 5 - Production/Stable",
-		"Topic :: Scientific/Engineering :: Image Processing",
-	],
+    name="pixelpainter",
+    version="0.2",
+    description="Convert images to HTML.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Pedro Torcatt",
+    author_email="pedrotorcattsoto@gmail.com",
+    url="https://github.com/Jalkhov/pixelpainter",
+    license="MIT",
+    keywords="paint html pixel convert image",
+    packages=['pixelpainter'],
+    install_requires=[
+        'numpy',
+        'pyperclip'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pixelpainter = pixelpainter.core:main',
+        ],
+    },
+    classifiers=[
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Image Processing",
+    ],
 )
